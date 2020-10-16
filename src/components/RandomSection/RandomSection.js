f4c5d426b4244ae338d685e49882338511da388c
nxxc <nomad.nxxc@gmail.com>
2020-10-16 17:39:40 +0900

    [fix]

--- src/components/RandomSection/RandomSection.js
+++ src/components/RandomSection/RandomSection.js
@@ -4,7 +4,7 @@ import {
   ImageSection,
   StateComponent,
 } from '../factory/componentFactory.js';
-import ImageTemplate from '../templates/ImageTemplate.js';
+import ImageTemplate from '../templates/ImageTemplate.js.js';
 import LoadingTemplate from '../templates/LoadingTemplate.js';
 
 export default class RandomSection extends ImageSection {
