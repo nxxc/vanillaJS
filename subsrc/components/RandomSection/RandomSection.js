0971c289279564be431b7b8cd1237d618913fef9
nxxc <nomad.nxxc@gmail.com>
2020-10-16 16:34:23 +0900

    [add] random slider

--- subsrc/components/RandomSection/RandomSection.js
+++ subsrc/components/RandomSection/RandomSection.js
@@ -13,54 +13,74 @@ export default class RandomSection extends ImageSection {
     this.state = {
       data: store.getRandomData().data,
     };
+
     this.randomSlide = new StateComponent({
       target: this.htmlTag,
       tag: 'section',
       className: 'random__banner',
     });
+
     this.prevBtn = new CustomBtn({
       target: this.htmlTag,
       tag: 'button',
       className: 'random__btn--prev',
-      onClick: () => {
-        console.log('prev');
-      },
+      onClick: this.onPrevBtnClick,
     });
     this.prevBtn.htmlTag.innerHTML = 'prev';
+
     this.nextBtn = new CustomBtn({
       target: this.htmlTag,
       tag: 'button',
       className: 'random__btn--next',
-      onClick: () => {
-        console.log('next');
-      },
+      onClick: this.onNextBtnClick,
     });
     this.nextBtn.htmlTag.innerHTML = 'next';
+
+    this.width = this.randomSlide.htmlTag.getBoundingClientRect().width;
+    this.styleSheet = document.styleSheets[0];
+    this.itemClass = this.styleSheet.cssRules[29];
 
     this.render();
   }
 
+  onPrevBtnClick = () => {
+    let currentRight = parseFloat(
+      this.itemClass.style.right.slice(0, this.itemClass.style.right.length - 2)
+    );
+    if (currentRight === 0) currentRight = this.width * 10;
+    this.itemClass.style.right = `${currentRight - this.width}px`;
+  };
+
+  onNextBtnClick = () => {
+    let currentRight = parseFloat(
+      this.itemClass.style.right.slice(0, this.itemClass.style.right.length - 2)
+    );
+    if (currentRight === this.width * 9) currentRight = -this.width;
+    this.itemClass.style.right = `${currentRight + this.width}px`;
+  };
+
   render() {
     if (this.state.isLoading) {
       this.randomSlide.htmlTag.innerHTML = LoadingTemplate;
-
-      return;
-    }
-    if (!this.state.isError) {
-      if (!this.state.data.length) {
-        this.randomSlide.htmlTag.innerHTML = '검색결과가 없습니다';
-      } else {
-        const data = this.state.data.slice(0, 5);
-        this.randomSlide.htmlTag.innerHTML = data
-          .map((cat, idx) => imageTemplate(cat, idx, 'random__banner--item'))
-          .join('');
-      }
+      this.randomSlide.htmlTag.classList.add('loading');
     } else {
-      this.randomSlide.htmlTag.innerHTML = `
+      this.randomSlide.htmlTag.classList.remove('loading');
+      if (!this.state.isError) {
+        if (!this.state.data.length) {
+          this.randomSlide.htmlTag.innerHTML = '검색결과가 없습니다';
+        } else {
+          // const data = this.state.data.slice(0, 5);
+          this.randomSlide.htmlTag.innerHTML = this.state.data
+            .map((cat, idx) => imageTemplate(cat, idx, 'random__banner--item'))
+            .join('');
+        }
+      } else {
+        this.randomSlide.htmlTag.innerHTML = `
         Error 발생!
         status:${this.state.status}
         message:${this.state.message}
         `;
+      }
     }
   }
 }
--- subsrc/components/templates/imageTemplate.js
+++ subsrc/components/templates/imageTemplate.js
@@ -2,12 +2,11 @@ const imageTemplate = (
   cat,
   idx,
   className
-) => `<article class=${className} data-id =${idx}>
-  <img src="${className === 'random__banner--item' ? cat.url : ''}" alt=${
+) => `<article class="${className}" data-id ="${idx}">
+  <img src="${className === 'random__banner--item' ? cat.url : ''}" 
+  alt="${cat.name}" data-id="${cat.id}" title="${
   cat.name
-}  data-id=${cat.id} title=${cat.name} class="image"
-  data-src=${cat.url}  
-  />
+}" class="image" data-src="${cat.url}"/>
   </article>`;
 
 export default imageTemplate;
